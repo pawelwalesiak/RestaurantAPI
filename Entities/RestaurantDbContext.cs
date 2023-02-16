f44a478f2e387b58d348e2d123db03c4ea901300
pawelwalesiak <s12828@pjwstk.edu.pl>
2023-02-16 19:43:42 +0100

    Add project files.

---
+++ Entities/RestaurantDbContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestaurantAPI.Entities
+{
+    public class RestaurantDbContext : DbContext
+    {
+        private string _connectionString = "Data Source=.\\;Initial Catalog=RestaurantDb;Integrated Security=True;Trusted_Connection=True;Encrypt=false";
+        //Data Source=.\;Initial Catalog=RestaurantDb;Integrated Security=True
+        //kontekst bazy danych 
+        //odwzorwoanie klas na tabele 
+        //publiczne wlasnicwosci ktore jak ogeneryczny parametr przyjmuja typk ktory bedzie prezentowqac dana tabele 
+
+        public DbSet<Restaurant> Restaurants { get; set; } 
+        public DbSet<Address> Addresses { get; set; }
+        public  DbSet<Dish> Dishes { get; set; }
+
+        // wymagane dane - dodatkowa konfiguracja nadpisana w metodzie on model creating 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+            modelBuilder.Entity<Restaurant>()
+                .Property(r => r.Name)
+                .IsRequired()
+                .HasMaxLength(21); 
+            
+            modelBuilder.Entity<Dish>()
+                .Property(r => r.Name)
+                .IsRequired()
+                .HasMaxLength(21);
+            modelBuilder.Entity<Address>() 
+                .Property(a=>a.City)
+                .IsRequired()
+                .HasMaxLength(50);
+            modelBuilder.Entity<Address>() 
+                .Property(a=>a.Street)
+                .IsRequired()
+                .HasMaxLength(50);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+
+using RestaurantAPI.Entities;
+
+namespace RestaurantAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //utowrzenie webhosta
+            //zostaje
+            var builder = WebApplication.CreateBuilder(args);
+            
+            // Add services to the container.
+            //metoda configure service zalezlonsci kontenea dep 
+            //rejestracja zaleznosci dependency innjeciotn w kontenerze
+
+            
+
+            builder.Services.AddTransient<IWeatherForcastService, WeatherForcastService>();
+            builder.Services.AddControllers();
+
+            builder.Services.AddScoped<RestaurantSeeder>();
+            builder.Services.AddDbContext<RestaurantDbContext>();
+            builder.Services.AddScoped<RestaurantSeeder>();
+            //recznie utworzyc scope zeby pobrac z kontnenera depdency injection
+            
+            
+            var app = builder.Build();
+            
+            // Configure the HTTP request pipeline.
+
+           //budowanie scope 
+           var scope = app.Services.CreateScope();
+           var seeder = scope.ServiceProvider.GetRequiredService<RestaurantSeeder>();
+
+          
+            app.UseHttpsRedirection();
+            //app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+            seeder.Seed();
+
+        }
+    }
+}
