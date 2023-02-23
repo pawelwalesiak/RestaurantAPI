36de2cc9881a4702584b1e9e402e9911da293a56
pawelwalesiak <s12828@pjwstk.edu.pl>
2023-02-23 17:20:59 +0100

    Add removeAll dishes

--- Services/DishService.cs
+++ Services/DishService.cs
@@ -9,6 +9,10 @@ namespace RestaurantAPI.Services
     public interface IDishService
     {
         int Create(int restaurantId, CreateDishDto dto);
+        DishDto GetById( int restaurantId, int dishId);
+        List<DishDto> GetAll(int restaurantId);
+
+        void RemoveAll(int restaurantId);   
     }
 
 
@@ -35,6 +39,49 @@ namespace RestaurantAPI.Services
             _context.SaveChanges();
             return dishEntity.Id;
 
+        }
+
+        public DishDto GetById(int restaurantId, int dishId)
+        {
+            var restaurant = GetRestaurantById(restaurantId);
+
+            var dish = _context.Dishes.FirstOrDefault(d => d.Id == dishId);
+
+            if (dish is null || dish.RestaurantId != restaurantId)
+            {
+                throw new NotFoundException("Dish not found");
+            }
+            //mapowanie do modelu ddto mapujemy z encji dish na model DTO jako parametr generyczny DishDto a zródło jest to encja dish;
+            var dishDto = _mapper.Map<DishDto>(dish);
+            return dishDto;
+        }
+
+        public List<DishDto> GetAll(int restaurantId)
+        {
+            var restaurant = GetRestaurantById(restaurantId); 
+
+            var dishDtos = _mapper.Map<List<DishDto>>(restaurant.Dishes);
+            return dishDtos;
+        }
+
+        public void RemoveAll(int restaurantId)
+        {
+            var restaurant = GetRestaurantById(restaurantId);
+
+            _context.RemoveRange(restaurant.Dishes);
+            _context.SaveChanges();
+        }
+
+        private Restaurant GetRestaurantById(int restaurantId)
+        {
+            var restaurant = _context.Restaurants
+                .Include(r => r.Dishes)
+                .FirstOrDefault(r => r.Id == restaurantId);
+
+
+            if (restaurant == null)
+                throw new NotFoundException("Restaurant not found");
+            return restaurant;
         }
     }
 }
