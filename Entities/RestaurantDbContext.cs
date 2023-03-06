634df5df0d77b133f57ca6d383874f33004920f0
pawelwalesiak <s12828@pjwstk.edu.pl>
2023-03-06 20:43:01 +0100

    Add user and role tables, db migration

--- Entities/RestaurantDbContext.cs
+++ Entities/RestaurantDbContext.cs
@@ -13,12 +13,22 @@ namespace RestaurantAPI.Entities
 
         public DbSet<Restaurant> Restaurants { get; set; } 
         public DbSet<Address> Addresses { get; set; }
-        public  DbSet<Dish> Dishes { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+        public DbSet<User> Users { get; set; }  
+        public DbSet<Role> Roles { get; set; }
 
         // wymagane dane - dodatkowa konfiguracja nadpisana w metodzie on model creating 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            
+            modelBuilder.Entity<User>()
+                .Property(u => u.Email)
+                .IsRequired();
+
+            modelBuilder.Entity<Role>()
+                .Property(u => u.Name)
+                .IsRequired();
+
+
             modelBuilder.Entity<Restaurant>()
                 .Property(r => r.Name)
                 .IsRequired()
