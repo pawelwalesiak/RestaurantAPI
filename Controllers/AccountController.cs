e4fbcb1ed675b004169b16f9ba9aa8439c9120f2
pawelwalesiak <s12828@pjwstk.edu.pl>
2023-03-06 21:54:12 +0100

    Add AccountController and service

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Identity.Client;
+using RestaurantAPI.Models;
+using RestaurantAPI.Services;
+using static RestaurantAPI.Services.AccountService;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/account")]
+    [ApiController]
+    //walidacja modelu kontrolera - automat 
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost("register")]
+        public ActionResult RegisterUser([FromBody] RegisterUserDto dto) 
+        {
+            //dodajemy model wysylany przez usera walidacje zalatwia appi controller 
+            _accountService.RegisterUser(dto);
+            return Ok();    
+        }
+
+
+
+    }
+}
---
+++ Models/RegisterUserDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantAPI.Models
+{
+    public class RegisterUserDto
+    {
+        public string? FirstName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        [MinLength(8)]
+        public string Password { get; set; }
+        public string Nationality { get; set; }
+        public DateTime? DateOfBirth { get; set; }
+
+        // id danej roli dla uzytkownika , domysle uzytkonik bedzie w roli user 
+        public int RoleId { get; set; } = 1;
+    }
+}
